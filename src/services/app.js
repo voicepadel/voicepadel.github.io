fedcdec657f93cfc47b5d9d6ec9eb79df099ea6b
agus <agustinolivieri30@gmail.com>
2024-11-03 20:56:52 -0300

    feature unirse a lobby modal

--- src/services/app.js
+++ src/services/app.js
@@ -5,6 +5,11 @@ document.getElementById('createLobby').addEventListener("click", function() {
     redirectTo("/src/views/lobby/lobby.html");
 });
 
+document.getElementById('modalBtn').addEventListener("click", function() {
+    const userInput = document.getElementById('modalInput').value
+    redirectTo(`/src/views/match/match.html?matchIdentifier=${userInput}`);
+});
+
 var myAccountElement = document.getElementById('myAccount');
 console.log(isLoggedIn())
 if (isLoggedIn()){
