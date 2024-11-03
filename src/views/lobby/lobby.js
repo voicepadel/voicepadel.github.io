404b0713d219e507c413dcfd8d708ff3dea23915
pedroberecibar <pedroberecibar.dev@gmail.com>
2024-11-03 20:57:13 -0300

    boton x headers

--- src/views/lobby/lobby.js
+++ src/views/lobby/lobby.js
@@ -2,80 +2,94 @@ import { redirectTo } from "../../services/redirector.js";
 
 // Script para manejar la selección del modo de juego
 document
-.getElementById("gameModeSelect")
-.addEventListener("change", function () {
-  const gameDescription = document.getElementById("gameDescription");
-  const partidoCorto = document.getElementById("partido-corto");
-  const partidoEstandar = document.getElementById("partido-estandar");
+  .getElementById("gameModeSelect")
+  .addEventListener("change", function () {
+    const gameDescription = document.getElementById("gameDescription");
+    const partidoCorto = document.getElementById("partido-corto");
+    const partidoEstandar = document.getElementById("partido-estandar");
 
-  // Ocultar todas las descripciones
-  partidoCorto.style.display = "none";
-  partidoEstandar.style.display = "none";
+    // Ocultar todas las descripciones
+    partidoCorto.style.display = "none";
+    partidoEstandar.style.display = "none";
 
-  // Mostrar la descripción adecuada según la opción seleccionada
-  switch (this.value) {
-    case "1":
-      partidoCorto.style.display = "block";
-      break;
-    case "2":
-      partidoEstandar.style.display = "block";
-      break;
-    default:
-      break;
-  }
-});
+    // Mostrar la descripción adecuada según la opción seleccionada
+    switch (this.value) {
+      case "1":
+        partidoCorto.style.display = "block";
+        break;
+      case "2":
+        partidoEstandar.style.display = "block";
+        break;
+      default:
+        break;
+    }
+  });
 
 const params = new URLSearchParams(window.location.search);
 
-const matchIdentifier = params.get('matchIdentifier');
+const matchIdentifier = params.get("matchIdentifier");
 
-document.getElementById('start-match').addEventListener('click', function() {
+document.getElementById("start-match").addEventListener("click", function () {
   // Obtener los valores de los campos de texto
-  const firstPlayerTeamA = document.getElementById('firstPlayerTeamA').value;
-  const secondPlayerTeamA = document.getElementById('secondPlayerTeamA').value;
-  const firstPlayerTeamB = document.getElementById('firstPlayerTeamB').value;
-  const secondPlayerTeamB = document.getElementById('secondPlayerTeamB').value;
+  const firstPlayerTeamA = document.getElementById("firstPlayerTeamA").value;
+  const secondPlayerTeamA = document.getElementById("secondPlayerTeamA").value;
+  const firstPlayerTeamB = document.getElementById("firstPlayerTeamB").value;
+  const secondPlayerTeamB = document.getElementById("secondPlayerTeamB").value;
 
-      // Verificar si los campos están vacíos
-  if (!firstPlayerTeamA && !secondPlayerTeamA && !firstPlayerTeamB && !secondPlayerTeamB) {
-      // Activar validación de Bootstrap
-      const firstField = document.getElementById('firstPlayerTeamA');
-      firstField.classList.add('is-invalid');
+  // Verificar si los campos están vacíos
+  if (
+    !firstPlayerTeamA &&
+    !secondPlayerTeamA &&
+    !firstPlayerTeamB &&
+    !secondPlayerTeamB
+  ) {
+    // Activar validación de Bootstrap
+    const firstField = document.getElementById("firstPlayerTeamA");
+    firstField.classList.add("is-invalid");
 
-      // Puedes mostrar un mensaje adicional si lo deseas
-      return; // Detener la ejecución
+    // Puedes mostrar un mensaje adicional si lo deseas
+    return; // Detener la ejecución
   } else {
-      // Limpiar la clase de error si ya hay un valor
-      document.getElementById('firstPlayerTeamA').classList.remove('is-invalid');
+    // Limpiar la clase de error si ya hay un valor
+    document.getElementById("firstPlayerTeamA").classList.remove("is-invalid");
   }
 
   // Armar el objeto para el body del fetch
   const data = {
-      FirstPlayerNameTeamA: firstPlayerTeamA,
-      SecondPlayerTeamA: secondPlayerTeamA,
-      FirstPlayerTeamB: firstPlayerTeamB,
-      SecondPlayerTeamB: secondPlayerTeamB
+    FirstPlayerNameTeamA: firstPlayerTeamA,
+    SecondPlayerTeamA: secondPlayerTeamA,
+    FirstPlayerTeamB: firstPlayerTeamB,
+    SecondPlayerTeamB: secondPlayerTeamB,
   };
 
   // Hacer el fetch
-  fetch('https://voice-paddel-fmgzeugkg5bjh6f9.brazilsouth-01.azurewebsites.net/api/match/startQuickMatch', {
-      method: 'POST',
+  fetch(
+    "https://voice-paddel-fmgzeugkg5bjh6f9.brazilsouth-01.azurewebsites.net/api/match/startQuickMatch",
+    {
+      method: "POST",
       headers: {
-          'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
-  })
-  .then(response => {
+      body: JSON.stringify(data),
+    }
+  )
+    .then((response) => {
       if (!response.ok) {
-          throw new Error('Error en la red');
+        throw new Error("Error en la red");
       }
       return response.json();
-  })
-  .then(data => {
-      console.log('Éxito:', data);
-      redirectTo('/src/views/match/match.html?matchIdentifier=' + data.matchIdentifier);
-  })
-  .catch((error) => {
-      console.error('Error:', error);
-  });
+    })
+    .then((data) => {
+      console.log("Éxito:", data);
+      redirectTo(
+        "/src/views/match/match.html?matchIdentifier=" + data.matchIdentifier
+      );
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
+document.getElementById("closeLobby").addEventListener("click", function () {
+  window.location.href = "../index.html";
 });
--- src/views/match/match.js
+++ src/views/match/match.js
@@ -1,13 +1,13 @@
-import { startSingalR } from '../../services/singalR.js';
+import { startSingalR } from "../../services/singalR.js";
 
 let seconds = 0,
-minutes = 0,
-hours = 0;
+  minutes = 0,
+  hours = 0;
 let timerInterval; // Variable para almacenar el intervalo del temporizador
 let isPaused = false; // Variable para controlar el estado del temporizador
 
 const urlParams = new URLSearchParams(window.location.search);
-const matchIdentifier = urlParams.get('matchIdentifier');
+const matchIdentifier = urlParams.get("matchIdentifier");
 let currentGameGuid = "";
 let useSignalR = true;
 let microphoneManuallyStopped = false;
@@ -20,7 +20,7 @@ connection.on("GetNewState", (message) => {
   setMatchPoints(message);
 });
 
-console.log('matchIdentifier: ' + matchIdentifier);
+console.log("matchIdentifier: " + matchIdentifier);
 
 function onLoad() {
   getMatchCurrentState();
@@ -28,215 +28,234 @@ function onLoad() {
 }
 
 function setMatchPoints(data) {
-    document.getElementById("pointsTeamA").innerText = data.currentPointsTeamA.toString().padStart(2, '0');
-    document.getElementById("pointsTeamB").innerText = data.currentPointsTeamB.toString().padStart(2, '0');
-    document.getElementById("pointsTeamA").innerText = data.currentPointsTeamA.toString().padStart(2, '0');
-    document.getElementById("pointsTeamB").innerText = data.currentPointsTeamB.toString().padStart(2, '0');
+  document.getElementById("pointsTeamA").innerText = data.currentPointsTeamA
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("pointsTeamB").innerText = data.currentPointsTeamB
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("pointsTeamA").innerText = data.currentPointsTeamA
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("pointsTeamB").innerText = data.currentPointsTeamB
+    .toString()
+    .padStart(2, "0");
 
-    document.getElementById("firstSetnumGamesTeamA").innerText = data.set1NumberOfGamesWonByTeamA ?? 0;
-    document.getElementById("secondSetnumGamesTeamA").innerText = data.set2NumberOfGamesWonByTeamA ?? 0;
-    document.getElementById("thirdSetnumGamesTeamA").innerText = data.set3NumberOfGamesWonByTeamA ?? 0;
+  document.getElementById("firstSetnumGamesTeamA").innerText =
+    data.set1NumberOfGamesWonByTeamA ?? 0;
+  document.getElementById("secondSetnumGamesTeamA").innerText =
+    data.set2NumberOfGamesWonByTeamA ?? 0;
+  document.getElementById("thirdSetnumGamesTeamA").innerText =
+    data.set3NumberOfGamesWonByTeamA ?? 0;
 
-    document.getElementById("firstSetnumGamesTeamB").innerText = data.set1NumberOfGamesWonByTeamB ?? 0;
-    document.getElementById("secondSetnumGamesTeamB").innerText = data.set2NumberOfGamesWonByTeamB ?? 0;
-    document.getElementById("thirdSetnumGamesTeamB").innerText = data.set3NumberOfGamesWonByTeamB ?? 0;
+  document.getElementById("firstSetnumGamesTeamB").innerText =
+    data.set1NumberOfGamesWonByTeamB ?? 0;
+  document.getElementById("secondSetnumGamesTeamB").innerText =
+    data.set2NumberOfGamesWonByTeamB ?? 0;
+  document.getElementById("thirdSetnumGamesTeamB").innerText =
+    data.set3NumberOfGamesWonByTeamB ?? 0;
 
-currentGameGuid = data.currentGameGuid;
+  currentGameGuid = data.currentGameGuid;
 }
 
 function getMatchCurrentState() {
-fetch(`https://voice-paddel-fmgzeugkg5bjh6f9.brazilsouth-01.azurewebsites.net/api/match/${matchIdentifier}/matchCurrentState`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
+  fetch(
+    `https://voice-paddel-fmgzeugkg5bjh6f9.brazilsouth-01.azurewebsites.net/api/match/${matchIdentifier}/matchCurrentState`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
     }
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Error en la solicitud');
-    }
-    return response.json();
-  })
-  .then(data => {
-    setMatchPoints(data);
-    console.log('Respuesta del servidor:', data);
-  })
-  .catch(error => {
-    console.error('Hubo un problema con la solicitud:', error);
-  });
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error en la solicitud");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setMatchPoints(data);
+      console.log("Respuesta del servidor:", data);
+    })
+    .catch((error) => {
+      console.error("Hubo un problema con la solicitud:", error);
+    });
 }
 
 function startTimer() {
-const timeElement = document.getElementById("time");
+  const timeElement = document.getElementById("time");
 
-timerInterval = setInterval(() => {
-  if (!isPaused) {
-    // Solo actualizar si no está en pausa
-    seconds++;
-    if (seconds === 60) {
-      seconds = 0;
-      minutes++;
+  timerInterval = setInterval(() => {
+    if (!isPaused) {
+      // Solo actualizar si no está en pausa
+      seconds++;
+      if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+      }
+      if (minutes === 60) {
+        minutes = 0;
+        hours++;
+      }
+
+      // Formato de tiempo HH:MM:SS
+      const formattedTime =
+        String(hours).padStart(2, "0") +
+        ":" +
+        String(minutes).padStart(2, "0") +
+        ":" +
+        String(seconds).padStart(2, "0");
+
+      timeElement.textContent = formattedTime;
     }
-    if (minutes === 60) {
-      minutes = 0;
-      hours++;
-    }
-
-    // Formato de tiempo HH:MM:SS
-    const formattedTime =
-      String(hours).padStart(2, "0") +
-      ":" +
-      String(minutes).padStart(2, "0") +
-      ":" +
-      String(seconds).padStart(2, "0");
-
-    timeElement.textContent = formattedTime;
-  }
-}, 1000);
+  }, 1000);
 }
 
 // Función para pausar el temporizador
 function pauseTimer() {
-isPaused = !isPaused; // Alternar el estado de pausa
-const pauseButton = document.getElementById("pauseButton");
-pauseButton.textContent = isPaused
-  ? "Reanudar Partido"
-  : "Pausar Partido"; // Cambiar texto del botón
+  isPaused = !isPaused; // Alternar el estado de pausa
+  const pauseButton = document.getElementById("pauseButton");
+  pauseButton.textContent = isPaused ? "Reanudar Partido" : "Pausar Partido"; // Cambiar texto del botón
 }
 
 // Iniciar el cronómetro al cargar la vista
 onLoad();
 
 // Agregar evento al botón de pausar
-document
-.getElementById("pauseButton")
-.addEventListener("click", pauseTimer);
+document.getElementById("pauseButton").addEventListener("click", pauseTimer);
 
-function countPoint(teamA){
+function countPoint(teamA) {
   let message = {
     matchName: matchIdentifier,
     setGameConcurrencyVersion: currentGameGuid,
-    pointTeamA: teamA
+    pointTeamA: teamA,
   };
 
-  console.log('count point:', message);
+  console.log("count point:", message);
 
   if (useSignalR) {
-    connection.invoke("CountPoint", message).catch(err => console.error(err));
-  }
-  else {
-    fetch(`https://voice-paddel-fmgzeugkg5bjh6f9.brazilsouth-01.azurewebsites.net/api/match/${matchIdentifier}/countPoint`, {
-        method: 'POST',
+    connection.invoke("CountPoint", message).catch((err) => console.error(err));
+  } else {
+    fetch(
+      `https://voice-paddel-fmgzeugkg5bjh6f9.brazilsouth-01.azurewebsites.net/api/match/${matchIdentifier}/countPoint`,
+      {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           setGameConcurrencyVersion: currentGameGuid,
-          pointTeamA: teamA
-        })
-      })
-      .then(response => {
+          pointTeamA: teamA,
+        }),
+      }
+    )
+      .then((response) => {
         if (!response.ok) {
-          throw new Error('Error en la solicitud');
+          throw new Error("Error en la solicitud");
         }
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         setMatchPoints(data);
       })
-      .catch(error => {
-        console.error('Hubo un problema con la solicitud:', error);
+      .catch((error) => {
+        console.error("Hubo un problema con la solicitud:", error);
       });
   }
 }
 
-document
-.getElementById("countPointTeamA")
-.addEventListener("click", () => {
-  countPoint(true)
+document.getElementById("countPointTeamA").addEventListener("click", () => {
+  countPoint(true);
 });
 
-document
-.getElementById("countPointTeamB")
-.addEventListener("click", () => {
-  countPoint(false)
+document.getElementById("countPointTeamB").addEventListener("click", () => {
+  countPoint(false);
 });
 
 setTestRecognition();
 
-function setTestRecognition(){
-    const phrasesAzul = ['azul', 'punto']
-    const phrasesRojo = ['rojo', 'punto']
+function setTestRecognition() {
+  const phrasesAzul = ["azul", "punto"];
+  const phrasesRojo = ["rojo", "punto"];
 
-    // Check for browser support
-    if (!('webkitSpeechRecognition' in window)) {
-        alert('Web Speech API is not supported by your browser.');
-    } else {
-        // Create a new instance of the webkitSpeechRecognition object
-        const recognition = new webkitSpeechRecognition();
+  // Check for browser support
+  if (!("webkitSpeechRecognition" in window)) {
+    alert("Web Speech API is not supported by your browser.");
+  } else {
+    // Create a new instance of the webkitSpeechRecognition object
+    const recognition = new webkitSpeechRecognition();
 
-        // Set continuous to true
-        recognition.continuous = true;
-        recognition.interimResults = false;
+    // Set continuous to true
+    recognition.continuous = true;
+    recognition.interimResults = false;
 
-        // Set language
-        recognition.lang = 'es-AR';
+    // Set language
+    recognition.lang = "es-AR";
 
-        // Handle results
-        recognition.onresult = function(event) {
-            let transcript = '';
-            for (let i = event.resultIndex; i < event.results.length; i++) {
-                transcript += event.results[i][0].transcript;
-            }
-            
-            if(phrasesAzul.every(p => transcript.toLowerCase().includes(p))) {
-                console.log('Punto reconocido azul')
-                countPoint(false)
-            }
-            
-            if(phrasesRojo.every(p => transcript.toLowerCase().includes(p))) {
-                console.log('Punto reconocido rojo')
-                countPoint(true)
-            }
-        };
+    // Handle results
+    recognition.onresult = function (event) {
+      let transcript = "";
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        transcript += event.results[i][0].transcript;
+      }
 
-        // Handle errors
-        recognition.onerror = function(event) {
-            console.error('Speech recognition error detected: ' + event.error);
-        };
+      if (phrasesAzul.every((p) => transcript.toLowerCase().includes(p))) {
+        console.log("Punto reconocido azul");
+        countPoint(false);
+      }
 
-        recognition.onaudiostart = () => {
-          showTurnOffMic();
-          console.log("El reconocimiento de voz ha comenzado a capturar el audio.");
-        };
+      if (phrasesRojo.every((p) => transcript.toLowerCase().includes(p))) {
+        console.log("Punto reconocido rojo");
+        countPoint(true);
+      }
+    };
 
-        recognition.onend = () => {
-          showTurnOnMic();
-          if(!microphoneManuallyStopped) {
-            console.log('Reconocimiento de voz detenido. Reiniciando...');
-            recognition.start(); // Reinicia el reconocimiento si se detiene
-          }
-        };
+    // Handle errors
+    recognition.onerror = function (event) {
+      console.error("Speech recognition error detected: " + event.error);
+    };
 
-        // Start recognition
-        document.getElementById('turnOnMic').addEventListener('click', function() {
-          microphoneManuallyStopped = false;
-          recognition.start();
-        });
+    recognition.onaudiostart = () => {
+      showTurnOffMic();
+      console.log("El reconocimiento de voz ha comenzado a capturar el audio.");
+    };
 
-        document.getElementById('turnOffMic').addEventListener('click', function() {
-          microphoneManuallyStopped = true;
-          recognition.stop();
-        });
-    }
+    recognition.onend = () => {
+      showTurnOnMic();
+      if (!microphoneManuallyStopped) {
+        console.log("Reconocimiento de voz detenido. Reiniciando...");
+        recognition.start(); // Reinicia el reconocimiento si se detiene
+      }
+    };
 
-    function showTurnOnMic() {
-      document.getElementById('turnOnMic').style.display = 'inline-block';
-      document.getElementById('turnOffMic').style.display = 'none';
-    }
+    // Start recognition
+    document.getElementById("turnOnMic").addEventListener("click", function () {
+      microphoneManuallyStopped = false;
+      recognition.start();
+    });
 
-    function showTurnOffMic() {
-      document.getElementById('turnOnMic').style.display = 'none';
-      document.getElementById('turnOffMic').style.display = 'inline-block';
-    }
+    document
+      .getElementById("turnOffMic")
+      .addEventListener("click", function () {
+        microphoneManuallyStopped = true;
+        recognition.stop();
+      });
+  }
+
+  function showTurnOnMic() {
+    document.getElementById("turnOnMic").style.display = "inline-block";
+    document.getElementById("turnOffMic").style.display = "none";
+  }
+
+  function showTurnOffMic() {
+    document.getElementById("turnOnMic").style.display = "none";
+    document.getElementById("turnOffMic").style.display = "inline-block";
+  }
 }
+
+document
+  .getElementById("confirmExitBtn")
+  .addEventListener("click", function () {
+    window.location.href = "../index.html";
+  });
